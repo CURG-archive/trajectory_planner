fca17a85d145182e1a5dafd503629f1f93aa390d
jon-weisz <jon.weisz@gmail.com>
2013-08-29 14:06:58 -0400

    made it possible to start as a node

--- manual_control.py
+++ manual_control.py
@@ -3,5 +3,8 @@ from trajectory_planner import *
 import IPython
 
 if __name__ == '__main__':
-    g = SetupStaubliEnv()
+    rospy.init_node('manual_control')
+    init_planner = rospy.get_param('init_planner')
+    g = SetupStaubliEnv(False, init_planner)
+
     IPython.embed()
