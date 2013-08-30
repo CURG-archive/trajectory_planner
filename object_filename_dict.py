4545e6e01be1d2830274cd97d7998f1ffbfd388a
jon-weisz <jon.weisz@gmail.com>
2013-08-30 16:19:17 -0400

    recovered file_name_dict from deleted test_object_grasps.py

---
+++ object_filename_dict.py
@@ -0,0 +1,55 @@
+
+rock_file_name = '/home/armuser/ros/RearmGraspit/cgdb/arm/darparock.iv'
+flask_file_name = '/home/armuser/ros/RearmGraspit/models/objects/flask.iv'
+drill_file_name = '/home/armuser/ros/RearmGraspit/cgdb/arm/darpadrill.iv'
+large_shaving_gel_file_name = '/home/armuser/ros/RearmGraspit/cgdb/model_database/gillette_shaving_gel.iv'
+coke_file_name = '/home/armuser/ros/RearmGraspit/cgdb/model_database/coke_can.iv'
+odwalla_file_name = '/home/armuser/ros/RearmGraspit/cgdb/model_database/odwalla_bottle.iv'
+all_file_name = '/home/armuser/ros/RearmGraspit/cgdb/model_database/all.iv'
+hammer_file_name = '/home/armuser/ros/RearmGraspit/cgdb/arm/darpahammer.iv'
+garnier_file_name =  '/home/armuser/ros/RearmGraspit/cgdb/model_database/garnier_shampoo_bottle.iv'
+flashlight_file_name = '/home/armuser/ros/RearmGraspit/cgdb/arm/darpaflashlight.iv'
+gillette_file_name = '/home/armuser/ros/RearmGraspit/cgdb/model_database/gillette_shaving_gel.iv'
+
+drill_custom_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/semantic/drill_custom_in_meters.iv'
+mug_custom_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/semantic/mug_custom_in_meters.iv'
+darpaphonehandset_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/semantic/darpaphonehandset_1000_different_coordinate_system.iv'
+
+box_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/ted/box_in_meters.iv'
+snapple_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/ted/snapple_in_meters.iv'
+library_cup_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/ted/library_cup_in_meters.iv'
+krylon_spray_file_name = '/home/armuser/ros/rearm/columbia_stacks/RearmGraspit/cgdb/ted/krylon_spray_in_meters.iv'
+milk_carton_file_name = '/home/armuser/ros/RearmGraspit/cgdb/model_database/milk_carton.ply'
+
+
+file_name_dict = dict()
+file_name_dict['18555'] = rock_file_name
+file_name_dict['18556'] = flask_file_name
+file_name_dict['18557'] = hammer_file_name
+file_name_dict['18558'] = drill_file_name
+file_name_dict['18716'] = large_shaving_gel_file_name
+file_name_dict['18722'] = coke_file_name
+file_name_dict['18707'] = odwalla_file_name
+file_name_dict['18650'] = all_file_name
+
+file_name_dict['garnier_shampoo_bottle'] = garnier_file_name
+file_name_dict['/garnier_shampoo_bottle'] = garnier_file_name
+file_name_dict['all'] = all_file_name
+file_name_dict['/all'] = all_file_name
+file_name_dict['odwalla_bottle'] = odwalla_file_name
+file_name_dict['darpaflashlight'] = flashlight_file_name
+file_name_dict['/darpaflashlight'] = flashlight_file_name
+file_name_dict['gillette_shaving_gel'] = gillette_file_name
+file_name_dict['/gillette_shaving_gel'] = gillette_file_name
+file_name_dict['milk_carton'] = milk_carton_file_name
+file_name_dict['/milk_carton'] = milk_carton_file_name
+file_name_dict['flask'] = flask_file_name
+
+file_name_dict['drill_custom'] = drill_custom_file_name
+file_name_dict['mug_custom'] = mug_custom_file_name
+file_name_dict['darpaphonehandset'] = darpaphonehandset_file_name
+file_name_dict['box'] = box_file_name
+file_name_dict['snapple'] = snapple_file_name
+file_name_dict['darparock'] = rock_file_name
+file_name_dict['krylon_spray'] = krylon_spray_file_name
+file_name_dict['library_cup'] = library_cup_file_name
