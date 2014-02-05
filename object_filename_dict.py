6095ecb899a88b8fd1e47a3b0e703c4f921acfd0
jvarley <armuser@quemoy.(none)>
2014-02-05 16:47:18 -0500

    removed flask since I do not have the .iv file for it, and made everything else depend on object_models rather than hardcoded armuser paths

--- object_filename_dict.py
+++ object_filename_dict.py
@@ -1,30 +1,38 @@
 
-rock_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/arm/darparock.iv'
-flask_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/models/objects/flask.iv'
-drill_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/arm/darpadrill.iv'
-large_shaving_gel_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/gillette_shaving_gel.iv'
-coke_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/coke_can.iv'
-odwalla_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/odwalla_bottle.iv'
-all_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/all.iv'
-hammer_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/arm/darpahammer.iv'
-garnier_file_name =  '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/garnier_shampoo_bottle.iv'
-flashlight_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/arm/darpaflashlight.iv'
-gillette_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/gillette_shaving_gel.iv'
+import roslib
+import roslib.packages
 
-drill_custom_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/semantic/drill_custom_in_meters.iv'
-mug_custom_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/semantic/mug_custom_in_meters.iv'
-darpaphonehandset_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/semantic/darpaphonehandset_1000_different_coordinate_system.iv'
+pkg_path = roslib.packages.get_pkg_dir('object_models')
 
-box_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/ted/box_in_meters.iv'
-snapple_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/ted/snapple_in_meters.iv'
-library_cup_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/ted/library_cup_in_meters.iv'
-krylon_spray_file_name = '/home/armuser/ros/rosbuild_src/rearm/columbia_stacks/RearmGraspit/cgdb/ted/krylon_spray_in_meters.iv'
-milk_carton_file_name = '/home/armuser/ros/rosbuild_src/RearmGraspit/cgdb/model_database/milk_carton.ply'
+dir_path = pkg_path + str('models/cgdb/arm_old/')
+rock_file_name = dir_path + str('darparock.iv')
+drill_file_name = dir_path + str('darpadrill.iv')
+hammer_file_name = dir_path + str('darpahammer.iv')
+flashlight_file_name = dir_path + str('darpaflashlight.iv')
+
+dir_path = pkg_path + str('models/cgdb/model_database/')
+large_shaving_gel_file_name = dir_path + str('gillette_shaving_gel.iv')
+coke_file_name = dir_path + str('coke_can.iv')
+odwalla_file_name = dir_path + str('odwalla_bottle.iv')
+all_file_name = dir_path + str('all.iv')
+garnier_file_name = dir_path + str('garnier_shampoo_bottle.iv')
+gillette_file_name = dir_path + str('gillette_shaving_gel.iv')
+milk_carton_file_name = dir_path + str('milk_carton.ply')
+
+dir_path = pkg_path + str('models/cgdb/semantic/')
+drill_custom_file_name = dir_path + str('drill_custom_in_meters.iv')
+mug_custom_file_name = dir_path + str('mug_custom_in_meters.iv')
+darpaphonehandset_file_name = dir_path + str('darpaphonehandset_1000_different_coordinate_system.iv')
+
+dir_path = pkg_path + str('models/cgdb/ted/')
+box_file_name = dir_path + str('box_in_meters.iv')
+snapple_file_name = dir_path + str('snapple_in_meters.iv')
+library_cup_file_name = dir_path + str('library_cup_in_meters.iv')
+krylon_spray_file_name = dir_path + str('krylon_spray_in_meters.iv')
 
 
 file_name_dict = dict()
 file_name_dict['18555'] = rock_file_name
-file_name_dict['18556'] = flask_file_name
 file_name_dict['18557'] = hammer_file_name
 file_name_dict['18558'] = drill_file_name
 file_name_dict['18716'] = large_shaving_gel_file_name
@@ -43,7 +51,6 @@ file_name_dict['gillette_shaving_gel'] = gillette_file_name
 file_name_dict['/gillette_shaving_gel'] = gillette_file_name
 file_name_dict['milk_carton'] = milk_carton_file_name
 file_name_dict['/milk_carton'] = milk_carton_file_name
-file_name_dict['flask'] = flask_file_name
 
 file_name_dict['drill_custom'] = drill_custom_file_name
 file_name_dict['mug_custom'] = mug_custom_file_name
