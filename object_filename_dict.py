69ddcdc583864082180543df0636786ff4858626
jvarley <armuser@quemoy.(none)>
2014-02-11 14:12:01 -0500

    Refactored creation of name dictionary to be more robust and report errors in finding the desired resources

--- object_filename_dict.py
+++ object_filename_dict.py
@@ -1,44 +1,48 @@
 
 import roslib
 import roslib.packages
+import rospy
 
-pkg_path = roslib.packages.get_pkg_dir('object_models')
+pkg_path = roslib.packages.get_pkg_dir('object_models') + '/'
 
-dir_path = pkg_path + str('models/cgdb/arm_old/')
-rock_file_name = dir_path + str('darparock.iv')
-drill_file_name = dir_path + str('darpadrill.iv')
-hammer_file_name = dir_path + str('darpahammer.iv')
-flashlight_file_name = dir_path + str('darpaflashlight.iv')
 
-dir_path = pkg_path + str('models/cgdb/model_database/')
-large_shaving_gel_file_name = dir_path + str('gillette_shaving_gel.iv')
-coke_file_name = dir_path + str('coke_can.iv')
-odwalla_file_name = dir_path + str('odwalla_bottle.iv')
-all_file_name = dir_path + str('all.iv')
-garnier_file_name = dir_path + str('garnier_shampoo_bottle.iv')
-gillette_file_name = dir_path + str('gillette_shaving_gel.iv')
-milk_carton_file_name = dir_path + str('milk_carton.ply')
 
-dir_path = pkg_path + str('models/cgdb/semantic/')
-drill_custom_file_name = dir_path + str('drill_custom_in_meters.iv')
-mug_custom_file_name = dir_path + str('mug_custom_in_meters.iv')
-darpaphonehandset_file_name = dir_path + str('darpaphonehandset_1000_different_coordinate_system.iv')
+def get_path(package_name, resource_name):
+    resources = roslib.packages.find_resource(package_name, resource_name)
+    if len(resources) == 0:
+        rospy.logerr("Failed to find resource %s in package %s"%(resource_name, package_name))
+        return ""
+    else:
+        return resources[0]
 
-dir_path = pkg_path + str('models/cgdb/ted/')
-box_file_name = dir_path + str('box_in_meters.iv')
-snapple_file_name = dir_path + str('snapple_in_meters.iv')
-library_cup_file_name = dir_path + str('library_cup_in_meters.iv')
-krylon_spray_file_name = dir_path + str('krylon_spray_in_meters.iv')
+
+rock_file_name = get_path('object_models','darparock.iv')
+drill_file_name = get_path('object_models','darpadrill.iv')
+hammer_file_name = get_path('object_models','darpahammer.iv')
+flashlight_file_name = get_path('object_models','darpaflashlight.iv')
+
+
+large_shaving_gel_file_name = get_path('object_models','gillette_shaving_gel.iv')
+coke_file_name = get_path('object_models','coke_can.iv')
+odwalla_file_name = get_path('object_models','odwalla_bottle.iv')
+all_file_name = get_path('object_models','all.iv')
+garnier_file_name = get_path('object_models','garnier_shampoo_bottle.iv')
+gillette_file_name = get_path('object_models','gillette_shaving_gel.iv')
+milk_carton_file_name = get_path('object_models','milk_carton.ply')
+
+
+drill_custom_file_name = get_path('object_models','drill_custom_in_meters.iv')
+mug_custom_file_name = get_path('object_models','mug_custom_in_meters.iv')
+darpaphonehandset_file_name = get_path('object_models','darpaphonehandset_1000_different_coordinate_system.iv')
+
+
+box_file_name = get_path('object_models','box_in_meters.iv')
+snapple_file_name = get_path('object_models','snapple_in_meters.iv')
+library_cup_file_name = get_path('object_models','library_cup_in_meters.iv')
+krylon_spray_file_name = get_path('object_models','krylon_spray_in_meters.iv')
 
 
 file_name_dict = dict()
-file_name_dict['18555'] = rock_file_name
-file_name_dict['18557'] = hammer_file_name
-file_name_dict['18558'] = drill_file_name
-file_name_dict['18716'] = large_shaving_gel_file_name
-file_name_dict['18722'] = coke_file_name
-file_name_dict['18707'] = odwalla_file_name
-file_name_dict['18650'] = all_file_name
 
 file_name_dict['garnier_shampoo_bottle'] = garnier_file_name
 file_name_dict['all'] = all_file_name
