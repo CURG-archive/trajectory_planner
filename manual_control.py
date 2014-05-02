0c91126f912dbc086f622ce504d4fdcedba74e80
jvarley <jakevarley@gmail.com>
2014-05-02 10:13:08 -0400

    renamed parameter

--- manual_control.py
+++ manual_control.py
@@ -4,7 +4,7 @@ import IPython
 
 if __name__ == '__main__':
     rospy.init_node('manual_control')
-    use_real_hw = rospy.get_param('use_real_hw')
-    g = SetupStaubliEnv(False, use_real_hw)
+    use_robot_hw = rospy.get_param('use_robot_hw')
+    g = SetupStaubliEnv(False, use_robot_hw)
 
     IPython.embed()
